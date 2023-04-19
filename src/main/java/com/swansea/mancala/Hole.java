11deb1b7080f06da13deaff72267ade2106798f2
Nathan <101812777+corndogit@users.noreply.github.com>
2023-04-19 17:41:35 +0100

    begin adding logic to board

--- src/main/java/com/swansea/mancala/Hole.java
+++ src/main/java/com/swansea/mancala/Hole.java
@@ -1,4 +1,21 @@
 package com.swansea.mancala;
 
 public class Hole {
+    protected int value;
+
+    public Hole(int value) {
+        this.value = value;
+    }
+
+    public void addPiece() {
+        this.value++;
+    }
+
+    public void removePiece() {
+        this.value--;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/main/java/com/swansea/mancala/MainApplication.java
+++ src/main/java/com/swansea/mancala/MainApplication.java
@@ -19,8 +19,8 @@ public class MainApplication extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("game-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
