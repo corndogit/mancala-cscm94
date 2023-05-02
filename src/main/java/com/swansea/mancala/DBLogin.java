a9414b3754d4fda4b17462e74143a3655b100990
Nathan <101812777+corndogit@users.noreply.github.com>
2023-05-02 19:01:47 +0100

    create enum for login info

---
+++ src/main/java/com/swansea/mancala/DBLogin.java
@@ -0,0 +1,13 @@
+package com.swansea.mancala;
+
+public enum DBLogin {
+    URL("jdbc:mysql://localhost:3306/User"),
+    USERNAME("root"),
+    PASSWORD("pass");
+
+    public final String label;
+
+    DBLogin(String label) {
+        this.label = label;
+    }
+}
