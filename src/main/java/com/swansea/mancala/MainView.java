662ac54be0949bbaeed5d1ced097251233fe6713
Nathan <101812777+corndogit@users.noreply.github.com>
2023-05-04 16:35:36 +0100

    readded main class

--- src/main/java/com/swansea/mancala/MainView.java
+++ src/main/java/com/swansea/mancala/MainView.java
@@ -11,15 +11,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * The main class for starting and rendering the application. It also provides the
- * controller for the main view that is displayed when Mancala is opened.
+ * It also provides the controller for the main view that is displayed when Mancala is opened.
  * @author Nathan Brenton
  * @version 1.0
  */
-public class MainView extends Application {
+public class MainView{
     public static User loggedInUser;
-    final int WINDOW_WIDTH = 800;
-    final int WINDOW_HEIGHT = 600;
     @FXML
     protected VBox mainWindow;
 
@@ -46,38 +43,17 @@ public class MainView extends Application {
 
     /**
      * Changes the view to the main menu of Mancala
+     *
      * @param mainWindow the Stage containing the main window
      */
     public void enterMainMenu(Stage mainWindow) throws IOException {
         FXMLLoader mainMenuLoader = new FXMLLoader(MainView.class.getResource("main-menu-view.fxml"));
         Scene scene = new Scene(
                 mainMenuLoader.load(),
-                WINDOW_WIDTH,
-                WINDOW_HEIGHT
+                Main.WINDOW_WIDTH,
+                Main.WINDOW_HEIGHT
         );
         mainWindow.setScene(scene);
         mainWindow.show();
-    }
-
-    /**
-     * Used for starting the application. Loads the relevant FXML files and renders them in a new window.
-     * @param stage used internally by JavaFX on launch
-     * @throws IOException if an error occurs during input or output operations
-     */
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainView.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), WINDOW_WIDTH, WINDOW_HEIGHT);
-        stage.setTitle("Mancala");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * Launches the application.
-     * @param args Command-line arguments
-     */
-    public static void main(String[] args) {
-        launch();
     }
 }
